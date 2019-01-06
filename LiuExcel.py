fc13c12bc0041d816cc8fd801a8091428b1596c4
thodsaphonsonthiphin <thodsaphonsp@gmail.com>
2019-01-06 20:18:43 +0700

    - add find case insensitive string

--- LiuExcel.py
+++ LiuExcel.py
@@ -2,9 +2,51 @@ import openpyxl
 from openpyxl import Workbook
 
 
-def loadWorkbook(name:str)->openpyxl.Workbook:
+def load_workbook(name:str)->openpyxl.Workbook:
+    '''
+    :param name: string include path file
+    :return: workbook from openpxl
+    '''
 
     return openpyxl.load_workbook(name)
+
+
+
+# my_workbook = load_workbook("mergeCell.xlsx")
+#
+# list_of_sheet_name = my_workbook.sheetnames
+#
+# sheet = my_workbook[list_of_sheet_name[0]]
+
+def search_case_insensitive(string_for_searching:str, workbook:Workbook,sheet_name:str, column:list)->list:
+    '''
+    find rows that have match string (not substhing)
+
+    :param string_for_searching: specific string for search
+    :param workbook: workbook from openpxl
+    :param sheet_name: sheet name from work book
+    :param column: specific column in work book
+    :return: list of row
+    '''
+    sheet = workbook[sheet_name]
+    my_list = []
+    for column_name in column:
+
+        #get a column in excel
+        local_cell_tuple = sheet[column_name]
+
+        for cell in local_cell_tuple:
+
+            # compare
+            if string_for_searching.lower() == str(cell.value).lower():
+
+                # get specific row from sheet
+                row = sheet[cell.row]
+
+                my_list.append(row)
+
+    return my_list
+
 
 
 
